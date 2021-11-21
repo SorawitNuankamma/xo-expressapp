606c9c070a2c5a4912ac3566c8b8a1363b937e82
Sorawit <sorawit.nu@ku.th>
2021-11-21 08:29:21 +0700

    baseline commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({message : 'hello from server',app : 'starter'})
+})
+
+const port = 3000;
+app.listen(port, ()=> {
+    console.log(`app running on port ${port}`);
+})
+
